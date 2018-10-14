2928e493b2eb1eee8f15947ddae5eea6be471da2
9schurke9 <sefagoevercin@hotmail.de>
2018-10-15 01:17:14 +0200

    Working on Anchoring!

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,11 +1,36 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Controller {
 
+   // @FXML
+  //  private Button createVisitButton;
+
+      @FXML
+      public Pane contentPane;
+
     @FXML
     public void initialize(){
+
+    }
+    @FXML
+    public void createVisitButtonAction(ActionEvent event){
+        try {
+
+            Parent form = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/Form.fxml"));
+            contentPane.getChildren().setAll(form);
+        }catch(IOException i){
+            i.printStackTrace();
+        }
 
     }
 }
