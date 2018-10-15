42046ac3817d8a8c328c87d4d934181ee75ebbf3
9Sefa9 <sefagoevercin@hotmail.de>
2018-10-15 18:00:13 +0200

    working on windwo scaling

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,7 +13,8 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("fxml/Main.fxml"));
         primaryStage.setTitle("VisID");
         primaryStage.setScene(new Scene(root, 1024 , 720));
-        primaryStage.setResizable(false);
+
+        primaryStage.setResizable(true);
         primaryStage.show();
     }
 
