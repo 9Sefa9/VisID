2ef1e2548909e9f1d44add7536311e2a144b5e60
9schurke9 <sefagoevercin@hotmail.de>
2018-10-16 00:21:04 +0200

    viele konfuse sachen gemacht lol

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,25 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
+
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Screen;
+
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         Parent root = FXMLLoader.load(getClass().getResource("fxml/Main.fxml"));
         primaryStage.setTitle("VisID");
         primaryStage.setScene(new Scene(root, 1024 , 720));
 
         primaryStage.setResizable(true);
         primaryStage.show();
-    }
 
+    }
 
     public static void main(String[] args) {
         launch(args);
--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,34 +1,43 @@
 package controller;
-
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Controller {
 
-   // @FXML
-  //  private Button createVisitButton;
+    private Parent form;
+    private Parent test;
 
-      @FXML
-      public Pane contentPane;
+    @FXML public Pane contentPane;
 
     @FXML
-    public void initialize(){
+    public void initialize()throws IOException{
 
     }
     //füge Form.fxml zu mid-main-pane hinzu
     @FXML
-    public void createVisitButtonAction(ActionEvent event){
-        Parent form;
+    public void visitButtonAction(){
         try {
+            //sorgt dafür, dass daten erhalten bleiben bei fxml scene wechsel
+            if(form == null)
            form = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/Form.fxml"));
-           contentPane.getChildren().setAll(form);
+
+            this.contentPane.getChildren().setAll(form);
+        }catch(IOException i){
+            i.printStackTrace();
+        }
+
+    }
+    @FXML
+    public void test(){
+        try {
+            if(test == null)
+            test = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/Test.fxml"));
+
+            this.contentPane.getChildren().setAll(test);
         }catch(IOException i){
             i.printStackTrace();
         }
