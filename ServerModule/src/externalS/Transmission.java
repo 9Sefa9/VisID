d712a558a2640ef11db0f1062e3f32123283feca
9Sefa9 <sefagoevercin@hotmail.de>
2018-11-03 17:15:42 +0100

    wokring on transmission

--- ServerModule/src/externalS/Transmission.java
+++ ServerModule/src/externalS/Transmission.java
@@ -1,14 +1,34 @@
 package externalS;
 
+import com.sun.istack.internal.logging.Logger;
+import sun.rmi.runtime.Log;
+
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
+import java.util.logging.Level;
 
 public class Transmission implements Runnable{
    private ServerSocket server;
    private Socket client;
+   private Logger logger = Logger.getLogger(this.getClass());
     @Override
     public void run() {
         while(true){
+            try {
+                client = server.accept();
+                logger.log(dLevel.INFO,""+client.getInetAddress()+" :: "+client.getLocalAddress()+" :: "+client.getPort());
+                new Thread(new InnerClassThread()).start();
+            }catch (IOException s){
+                s.printStackTrace();
+            }
+        }
+    }
+
+    private class InnerClassThread implements Runnable {
+        @Override
+        public void run() {
 
         }
     }
